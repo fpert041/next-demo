67d37671544ce39800d48a4c586fb4d9bcedf284
fpert041 <23483149+fpert041@users.noreply.github.com>
2021-07-29 15:32:03 +0100

    new home

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,8 @@
 import '../styles/globals.css'
 
+//import awsExports from '../components/aws-exports'
+//Amplify.configure({ ...awsExports, ssr: true })
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
